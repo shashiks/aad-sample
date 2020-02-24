bf1728a628bf6dcbbaf3d43686a92cfeb01e318b
Shashikant Soni <soni.shashikant@gmail.com>
2020-02-24 14:17:00 +0530

    Update todoListSvc.js

--- ad-spring-boot-client/App/Scripts/todoListSvc.js
+++ ad-spring-boot-client/App/Scripts/todoListSvc.js
@@ -3,16 +3,16 @@ angular.module('todoApp')
     .factory('todoListSvc', ['$http', function ($http) {
         return {
             getItems: function () {
-                return $http.get('http://localhost:9999/cirrus/api/todolist/');
+                return $http.get('http://localhost:9999/myapp/api/todolist/');
             },
             getItem: function (id) {
-                return $http.get('http://localhost:9999/cirrus/api/todolist/' + id);
+                return $http.get('http://localhost:9999/myapp/api/todolist/' + id);
             },
             postItem: function (item) {
-                return $http.post('http://localhost:9999/cirrus/api/todolist/', item);
+                return $http.post('http://localhost:9999/myapp/api/todolist/', item);
             },
             putItem: function (item) {
-                return $http.put('http://localhost:9999/cirrus/api/todolist/', item);
+                return $http.put('http://localhost:9999/myapp/api/todolist/', item);
             },
             deleteItem: function (id) {
                 return $http({
